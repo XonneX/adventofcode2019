941fb24eed09c42aaa7c0fa1621911ce4555d030
XonneX <baumannjanik@gmail.com>
2019-12-03 12:51:41 +0100

    Added day 3 part 2

--- src/XonneX/AdventOfCode2019/Day3/Part2/Day3Part2.php
+++ src/XonneX/AdventOfCode2019/Day3/Part2/Day3Part2.php
@@ -12,6 +12,77 @@ class Day3Part2 implements PuzzleInterface
 
     public function solve(string $input): string
     {
-        // TODO: Implement solve() method.
+        $wires = explode("\n", $input);
+        $grid = [];
+
+        foreach ($wires as $key => $wire) {
+            $instructions = explode(',', $wire);
+            $x = 0;
+            $y = 0;
+            $wireLength = 0;
+
+            foreach ($instructions as $instruction) {
+                $direction = substr($instruction, 0, 1);
+                $length = substr($instruction, 1);
+
+                if ($direction === 'R') {
+                    while ($length > 0) {
+                        $wireLength++;
+                        $x++;
+                        $grid[$x][$y][$key] = $wireLength;
+
+                        $length--;
+                    }
+                } elseif ($direction === 'L') {
+                    while ($length > 0) {
+                        $wireLength++;
+                        $x--;
+                        $grid[$x][$y][$key] = $wireLength;
+
+                        $length--;
+                    }
+                } elseif ($direction === 'U') {
+                    while ($length > 0) {
+                        $wireLength++;
+                        $y++;
+                        $grid[$x][$y][$key] = $wireLength;
+
+                        $length--;
+                    }
+                } elseif ($direction === 'D') {
+                    while ($length > 0) {
+                        $wireLength++;
+                        $y--;
+                        $grid[$x][$y][$key] = $wireLength;
+
+                        $length--;
+                    }
+                }
+            }
+        }
+
+        $intersections = [];
+
+        foreach ($grid as $x => $row) {
+            foreach ($row as $y => $value) {
+                if (count($value) !== count($wires)) {
+                    continue;
+                }
+
+                $intersections[] = $value;
+            }
+        }
+
+        $minSteps = PHP_INT_MAX;
+
+        foreach ($intersections as $intersection) {
+            $tmp = $intersection[0] + $intersection[1];
+
+            if ($tmp < $minSteps) {
+                $minSteps = $tmp;
+            }
+        }
+
+        return (string)$minSteps;
     }
 }
